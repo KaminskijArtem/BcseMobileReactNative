e2ff96d448004974045f958d35cd27b3688a07d6
Unknown <artemkaminskij2016@gmail.com>
2018-01-13 22:07:37 +0200

    Тестовая навигация

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, StatusBar } from 'react-native'
 import { Provider } from 'react-redux'
 import createStore from './Redux'
+/*
+* Both of the following files work for react-navigation
+* Routes will always be added and supported by modifying
+* the AppNavigation file.  Special redux actions/reducers
+* will be handled in Redux Navigation
+*   // use this to use react-navigation no redux
+*   import AppNavigation from './Navigation/AppNavigation'
+*
+*   // use this to use react-navigation with redux
+*   import ReduxNavigation from './Navigation/ReduxNavigation'
+*/
 
+// We're going to use navigation with redux
 import ReduxNavigation from './Navigation/ReduxNavigation'
 
+// create our store
 const store = createStore()
 
 
@@ -13,6 +26,7 @@ export default class App extends React.Component {
     return (
       <Provider store={store}>
         <View style={styles.container}>
+          <StatusBar barStyle='light-content' />
           <ReduxNavigation />
         </View>
       </Provider>
@@ -23,8 +37,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 })
